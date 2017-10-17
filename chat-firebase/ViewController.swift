6cee6357078324a4bf9281d198d0bc4ea8e5424b
Rupesh Mittal <Rupesh@Anishs-MacBook-Pro.local>
2017-10-17 16:12:06 +0530

    git initialized

---
+++ chat-firebase/ViewController.swift
@@ -0,0 +1,192 @@
+//
+//  ViewController.swift
+//  chat-firebase
+//
+//  Created by Rupesh Mittal on 13/10/17.
+//  Copyright © 2017 Rupesh Mittal. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import JSQMessagesViewController
+
+class ViewController: JSQMessagesViewController {
+  
+    var ref: DatabaseReference!
+    var messages = [JSQMessage]()
+    
+    lazy var outgoingBubbleImageView: JSQMessagesBubbleImage = self.setupOutgoingBubble()
+    lazy var incomingBubbleImageView: JSQMessagesBubbleImage = self.setupIncomingBubble()
+    
+    private  var messageRef: DatabaseReference?
+    private var newMessageRefHandle: DatabaseHandle?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("view did load")
+        ref = Database.database().reference()
+        messageRef = ref.child("users")
+        self.senderId = "1234"
+        self.senderDisplayName = "mittal"
+//        self.ref.child("users").setValue(["username": "hello"])
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.inputToolbar.contentView.leftBarButtonItem = nil
+        self.inputToolbar.contentView.backgroundColor = UIColor.white
+        self.inputToolbar.contentView.textView.layer.borderWidth = 0.0
+        // No avatars
+        collectionView!.collectionViewLayout.incomingAvatarViewSize = CGSize.zero
+        collectionView!.collectionViewLayout.outgoingAvatarViewSize = CGSize.zero
+
+        observeMessages()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("view will appear")
+//        self.ref.child("users").setValue(["username1": "hello"])
+        
+        // messages from someone else
+        addMessage(withId: "foo", name: "Mr.Bolt", text: "I am so fast!")
+        // messages sent from local sender
+        addMessage(withId: senderId, name: "Me", text: "I bet I can run faster than you!")
+        addMessage(withId: senderId, name: "Me", text: "I like to run!")
+        // animates the receiving of a new message on the view
+        finishReceivingMessage()
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageDataForItemAt indexPath: IndexPath!) -> JSQMessageData! {
+        return messages[indexPath.item]
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    private func setupOutgoingBubble() -> JSQMessagesBubbleImage {
+        let bubbleImageFactory = JSQMessagesBubbleImageFactory()
+        return bubbleImageFactory!.outgoingMessagesBubbleImage(with: UIColor.jsq_messageBubbleBlue())
+    }
+    
+    private func setupIncomingBubble() -> JSQMessagesBubbleImage {
+        let bubbleImageFactory = JSQMessagesBubbleImageFactory()
+        return bubbleImageFactory!.incomingMessagesBubbleImage(with: UIColor.jsq_messageBubbleLightGray())
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAt indexPath: IndexPath!) -> JSQMessageBubbleImageDataSource! {
+        let message = messages[indexPath.item] // 1
+        if message.senderId == senderId { // 2
+            return outgoingBubbleImageView
+        } else { // 3
+            return incomingBubbleImageView
+        }
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAt indexPath: IndexPath!) -> JSQMessageAvatarImageDataSource! {
+        return nil
+    }
+    
+    
+    private func addMessage(withId id: String, name: String, text: String) {
+        if let message = JSQMessage(senderId: id, displayName: name, text: text) {
+            messages.append(message)
+        }
+    }
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = super.collectionView(collectionView, cellForItemAt: indexPath) as! JSQMessagesCollectionViewCell
+        let message = messages[indexPath.item]
+        
+        if message.senderId == senderId {
+            cell.textView?.textColor = UIColor.white
+        } else {
+            cell.textView?.textColor = UIColor.black
+        }
+        return cell
+    }
+    
+    override func didPressSend(_ button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: Date!) {
+        let itemRef = messageRef?.childByAutoId() // 1
+        let messageItem = [ // 2
+            "senderId": senderId!,
+            "senderName": senderDisplayName!,
+            "text": text!,
+            ]
+        
+        isTyping = false
+        itemRef?.setValue(messageItem) // 3
+        
+        JSQSystemSoundPlayer.jsq_playMessageSentSound() // 4
+        
+        finishSendingMessage() // 5
+    }
+    
+    private func observeMessages() {
+        messageRef = ref!.child("messages")
+        // 1.
+        let messageQuery = messageRef?.queryLimited(toLast:25)
+        
+        // 2. We can use the observe method to listen for new
+        // messages being written to the Firebase DB
+        newMessageRefHandle = messageQuery?.observe(.childAdded, with: { (snapshot) -> Void in
+            // 3
+            let messageData = snapshot.value as! Dictionary<String, String>
+            
+            if let id = messageData["senderId"] as String!, let name = messageData["senderName"] as String!, let text = messageData["text"] as String!, text.characters.count > 0 {
+                // 4
+                self.addMessage(withId: id, name: name, text: text)
+                
+                // 5
+                self.finishReceivingMessage()
+            } else {
+                print("Error! Could not decode message data")
+            }
+        })
+    }
+    
+    
+    override func textViewDidChange(_ textView: UITextView) {
+        super.textViewDidChange(textView)
+        // If the text is not empty, the user is typing
+//        print(textView.text != "")
+        isTyping = textView.text != ""
+        
+    }
+    
+    
+    private lazy var userIsTypingRef: DatabaseReference =
+        self.ref!.child("typingIndicator").child(self.senderId) // 1
+    private var localTyping = false // 2
+    var isTyping: Bool {
+        get {
+            return localTyping
+        }
+        set {
+            // 3
+            localTyping = newValue
+            userIsTypingRef.setValue(newValue)
+        }
+    }
+    
+    
+    private func observeTyping() {
+        let typingIndicatorRef = ref!.child("typingIndicator")
+        userIsTypingRef = typingIndicatorRef.child(senderId)
+        userIsTypingRef.onDisconnectRemoveValue()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        observeTyping()
+    }
+    
+}
+
+
